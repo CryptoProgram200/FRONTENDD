9bbf790922b9357d32ae23d1caf4f6f767ec28a8
Raunak0713 <raunakbhanarkar7@gmail.com>
2023-11-23 11:02:43 +0530

    apin

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import axios from "axios";
 import { Provider } from "react-redux";
 import { store } from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-axios.defaults.baseURL = "http://localhost:7080";
+axios.defaults.baseURL = "https://zany-lime-gosling-shoe.cyclic.app";
 root.render(
   <React.StrictMode>
     <BrowserRouter>
